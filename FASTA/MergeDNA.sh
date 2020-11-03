3aa5bf9b059440d136dbcc1a025ede6ca5e50f32
Pratas <pratas@ua.pt>
2020-11-03 17:59:01 +0000

    merge & split

---
+++ FASTA/MergeDNA.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# ==============================================================================
+#
+INPUT="$1";    # "Filename"
+SIZE="$2";     # "250MB"
+THREADS="$3";  # "8"
+#
+# ==============================================================================
+#
+tar -xvf $INPUT 1> .DEC_F_JV2;
+#
+mapfile -t FILES < .DEC_F_JV2;
+#
+IDX=1;
+D_NAMES="";
+#
+for file in "${FILES[@]}" #
+  do
+  ./JARVIS2 -d $file 2> .tmp_report_$file &
+  if [[ "$IDX" -eq "$THREADS" ]] || [[ "${#FILES[@]}" -eq "$IDX" ]]
+    then
+    wait;
+    fi
+  ((++IDX));  
+  done
+#
+rm -f $INPUT.out;
+for file in "${FILES[@]}" #
+  do
+  cat $file.jd >> $INPUT.out;
+  rm -f $file.jd $file .tmp_report_$file
+  done
+#
+rm -f .DEC_F_JV2 
+#
+# ==============================================================================
---
+++ FASTA/SplitDNA.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# ==============================================================================
+#
+INPUT="$1";
+SIZE="$2";
+THREADS="$3";
+#
+# ==============================================================================
+#
+split --bytes=$SIZE --numeric-suffixes=10 --verbose $INPUT $INPUT- \
+| tr -d "\'" | awk '{ print $3;}' > .ENC_F_JV2 2> .tmp_report;
+#
+mapfile -t FILES < .ENC_F_JV2;
+#
+C_NAMES="";
+IDX=1;
+#
+for file in "${FILES[@]}" #
+  do
+  ./JARVIS2 -l 9 $file 2> .tmp_report_$file &
+  if [[ "$IDX" -eq "$THREADS" ]] || [[ "${#FILES[@]}" -eq "$IDX" ]]
+    then
+    wait;
+    fi
+  C_NAMES+=" $file.jc ";
+  ((++IDX));
+  done
+#
+tar -cvf $INPUT.tar $C_NAMES 1>> .tmp_report 2>> .tmp_err; 
+for file in "${FILES[@]}" #
+  do
+  rm -f $file.jc $file .tmp_report_$file;
+  done
+#
+rm -f .ENC_F_JV2 .tmp_report .tmp_err;
+#
+# ==============================================================================
