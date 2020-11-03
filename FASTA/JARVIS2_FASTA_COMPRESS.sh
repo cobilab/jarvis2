6f116a2e4ea96bfb7b40775e7036eec3bd5c287c
Pratas <pratas@ua.pt>
2020-11-03 23:04:50 +0000

    info & fix

--- FASTA/JARVIS2_FASTA_COMPRESS.sh
+++ FASTA/JARVIS2_FASTA_COMPRESS.sh
@@ -8,16 +8,17 @@ INPUT="$1";
 [ ! -f $INPUT ] && { echo "$0: file $INPUT not found."; exit 2; }
 #
 ./SplitFastaStreams < $INPUT
-./SplitDNA.sh "DNA.JV2" "20MB" "8" &
+./SplitDNA.sh "DNA.JV2" "70MB" "8" &
 ./bbb cfm10q HEADERS.JV2 HEADERS.JV2.bbb &
 bzip2 -f EXTRA.JV2 &
 wait
-tar -cvf $INPUT.tar DNA.JV2.tar EXTRA.JV2.bz2 HEADERS.JV2.bbb
+tar -cvf $INPUT.tar DNA.JV2.tar EXTRA.JV2.bz2 HEADERS.JV2.bbb 1> .rep_out_enc
 #
-ls -la DNA.JV2.tar | awk '{ print "DNA:\t"$5; }'
-ls -la HEADERS.JV2.bbb | awk '{ print "HEADS:\t"$5; }'
-ls -la EXTRA.JV2.bz2 | awk '{ print "EXTRA:\t"$5; }'
-ls -la $INPUT.tar | awk '{ print "TOTAL:\t"$5; }'
+ls -lah HEADERS.JV2.bbb | awk '{ print "HEADS:\t"$5; }'
+ls -lah DNA.JV2.tar | awk '{ print "DNA:\t"$5; }'
+ls -lah EXTRA.JV2.bz2 | awk '{ print "EXTRA:\t"$5; }'
+echo "------";
+ls -lah $INPUT.tar | awk '{ print "TOTAL:\t"$5; }'
 #
-rm -f DNA.JV2.tar EXTRA.JV2 HEADERS.JV2 
+rm -f DNA.JV2.tar EXTRA.JV2 HEADERS.JV2 .rep_out_enc
 #
--- FASTA/JARVIS2_FASTA_DECOMPRESS.sh
+++ FASTA/JARVIS2_FASTA_DECOMPRESS.sh
@@ -7,12 +7,12 @@ INPUT="$1";
 # Make sure file exits else die
 [ ! -f $INPUT ] && { echo "$0: file $INPUT not found."; exit 2; }
 #
-tar -xvf $INPUT
-./MergeDNA.sh "DNA.JV2.tar" "20MB" "8" &
+tar -xvf $INPUT 1> .rep_out_dec
+./MergeDNA.sh "DNA.JV2.tar" "70MB" "8" &
 bunzip2 -f EXTRA.JV2.bz2 &
 ./bbb -fqd HEADERS.JV2.bbb HEADERS.JV2 &
 wait
-mv DNA.JV2.jc.jd DNA.JV2
+mv DNA.JV2.tar.out DNA.JV2
 ./MergeFastaStreams > $INPUT.out
-rm -f DNA.JV2.jc DNA.JV2.jc.jd EXTRA.JV2.bz2 HEADERS.JV2.bbb
+rm -f DNA.JV2.jc DNA.JV2.tar.out EXTRA.JV2.bz2 HEADERS.JV2.bbb .rep_out_dec
 #
